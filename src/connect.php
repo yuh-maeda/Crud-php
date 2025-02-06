efc76883daba300520a5383d1af3df692dec5c2c
yuh-maeda <yurimaedanick019@gmail.com>
2025-02-06 17:10:48 -0400

    crud atualizado, home finalizada

--- src/connect.php
+++ src/connect.php
@@ -3,12 +3,15 @@
 $hostname = '10.38.0.81';
 $username = "user";
 $password = '321';
-$db = 'crud_php';
+$db = 'crud_music';
 
 $conn = new mysqli($hostname,$username,$password,$db);
 
 if (!$conn){
-    echo 'Erro ao conectar !';
+    echo '<script> alert("Erro ao conectar")</script>';
+}
+else{
+    echo'<script> alert("conectado")</script>';
 }
 
 ?>
---
+++ src/musica.php
@@ -0,0 +1,83 @@
+<?php
+    require 'connect.php';
+    $sql = 'SELECT * FROM musica';
+    $result = mysqli_query($conn,$sql);
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Músicas</title>
+
+    <link rel="stylesheet" href="../styles/style.css">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" integrity="sha512-Evv84Mr4kqVGRNSgIGL/F/aIDqQb7xQ2vcrdIwxfjThSH8CSR7PBEakCr51Ck+w+/U6swU2Im1vVX0SVk9ABhg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+</head>
+
+
+
+<body>
+
+    <header>
+        <nav>
+            <ul>
+                <li class='menu-home'><a href="index.php"><i class="fa-solid fa-house"></i></a> <p>Home</p> </li>
+                <li class='menu-musica'><a href="musica.php"><i class="fa-solid fa-music"></i></a> <p>Musicas</p> </li>
+                <li class='menu-cantor'><a href="cantor.php"><i class="fa-solid fa-headphones"></i></a> <p>Cantores</p> </li>
+                <li class='menu-podcast'><a href="podcast.php"><i class="fa-solid fa-microphone"></i></a> <p>Podcast</p> </li>
+            </ul>
+        </nav>
+    </header>
+    
+    <main>
+        <section id='musica'>
+            <h1>Lista de Músicas</h1>
+            <div class="container">
+                <div class="area-tabela">
+                        <table class="tabela-musica">
+                            <thead class="default-thead">
+                                <tr class="default-tr">
+                                    <th class="default-th">Id</th>
+                                    <th class="default-th">Nome</th>
+                                    <th class="default-th">Autor</th>
+                                    <th class="default-th">Plays</th>
+                                    <th class="default-th">Duração</th>
+                                    <th class="default-th"></th>
+                                    <th class="default-th"></th>   
+                                </tr>
+                            </thead>
+
+                            <?php
+                                if ($result){
+                                    while($row = mysqli_fetch_assoc($result)){
+                                        $id = $row['id'];
+                                        $nome = $row['nome'];
+                                        $autor = $row['autor'];
+                                        $plays = $row['plays'];
+                                        $duracao = $row['duracao'];
+
+                                        echo '
+                                        <tr>
+                                            <td>'.$id.'</td>
+                                            <td>'.$nome.'</td>
+                                            <td>'.$autor.'</td>
+                                            <td>'.$plays.'</td>
+                                            <td>'.$duracao.'</td>
+                                            <td><a class= "btn btn-primary" href="editar_musica.php?id='.$id.'" >Editar</a></td>
+                                            <td><a class= "btn btn-danger" href="excluir_musica.php?id='.$id.'" >Excluir</a></td>
+                                        </tr>
+                                    ';
+                                    }
+                                }
+                            ?>
+                        </table>
+                </div>
+            </div>
+        </section>
+    </main>
+</body>
+</html>
